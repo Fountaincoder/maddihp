964f3769aa4aa36843f330b1de9ddff6d85cb618
Jonny Edwards <jonny@temporalcomputing.com>
2024-10-29 15:31:15 +0000

    added all the old shtuff

--- src/project.v
+++ src/project.v
@@ -1,27 +1,41 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Jonny Edwards
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none
-
-module tt_um_example (
+module tt_um_fountaincoder_top_V2 (
+    input  wire       clk,      // clock
+    input  wire       ena,      // will go high when the design is enabled
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+// All output pins must be assigned. If not used, assign to 0.
+assign uio_out[3:0] = 4'b0;
+/* verilator lint_off UNUSED */
+wire [3:0] dummy1 = uio_in[7:4];
+wire dummy2 = ena;
+assign uio_oe       = 8'b11110000;
+//assign all the loose ports
+wire [7:0] out;
+wire [3:0] out_top;
+assign uo_out = out;
+assign uio_out[7:4] = out_top;
+DMADD madd(
+        .clk    (clk),
+        .run    (uio_in[3]),
+        .load   (uio_in[2]),
+        .insn   (uio_in[1:0]),
+        .index  (ui_in[7:4]),
+        .data   (ui_in[3:0]),
+        .out    (out),
+        .out_top (out_top), 
+        .rst_n  (rst_n)
+);
 
 endmodule
+
--- test/test.py
+++ test/test.py
@@ -1,40 +1,284 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
+# SPDX-License-Identifier: MIT
 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+import random
+
+# DMADD madd(
+        # .clk    (clk),
+        # .run    (uio_in[3]),
+        # .load   (uio_in[2]),
+        # .insn   (uio_in[1:0]),
+        # .index  (ui_in[7:4]),
+        # .data   (ui_in[3:0]),
+        # .out    ({uio_out[7:4],uo_out}),
+        # .rst_n  (rst_n)
+# );
+
+@cocotb.test()
+async def test_mn1(dut):
+    dut._log.info("min 1")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    clock = Clock(dut.clk, 20, units="us")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk,10)
+    dut.ena.value  = 1
+    dut.uio_in.value = 0b0000_0_1_00
+    dut.ui_in.value = 0b0001_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value  = 0b0000_1_0_00
+    await ClockCycles(dut.clk,30 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==1
+
+@cocotb.test()
+async def test_mn12(dut):
+    dut._log.info("min 12")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    dut.rst_n.value = 1
+    dut.ena.value = 1
+    dut.uio_in.value = 0b0000_0_1_00
+    dut.ui_in.value = 0b1100_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_00
+    await ClockCycles(dut.clk,20 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value == 12
+
+@cocotb.test()
+async def test_mn78(dut):
+    dut._log.info("min 78")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    await ClockCycles(dut.clk,10)
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk,1)
+    dut.uio_in.value = 0b0000_0_1_00
+    dut.ui_in.value = 0b0111_0000
+    await ClockCycles(dut.clk,1 )
+    dut.ui_in.value = 0b1000_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_00
+    await ClockCycles(dut.clk,30 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==7
+ 
+@cocotb.test()
+async def test_mn8(dut):
+    dut._log.info("min 8")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    dut.uio_in.value = 0b0000_0_1_00
+    dut.ui_in.value = 0b1000_0000
+    await ClockCycles(dut.clk,10 )
+    dut.uio_in.value = 0b0000_1_0_00
+    await ClockCycles(dut.clk,30 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==8
+# 
+# #
+# # #######################################
+# #
+# 
+@cocotb.test()
+async def test_mx1(dut):
+    dut._log.info("max 1")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk,1)
+    dut.ena.value = 1
+    dut.uio_in.value = 0b0000_0_1_01
+    dut.ui_in.value = 0b0001_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_01
+    await ClockCycles(dut.clk,30 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==1
+
+@cocotb.test()
+async def test_mx12(dut):
+    dut._log.info("max 12")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    dut.rst_n.value = 1
+    dut.ena.value = 1
+    dut.uio_in.value = 0b0000_0_1_01
+    dut.ui_in.value = 0b1100_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_01
+    await ClockCycles(dut.clk,20 )
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==12
+
+@cocotb.test()
+async def test_mx78(dut):
+    dut._log.info("max 78")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    dut.uio_in.value = 0b0000_0_1_01
+    dut.ui_in.value = 0b0111_0000
+    await ClockCycles(dut.clk,1 )
+    dut.ui_in.value = 0b1000_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_01
+    await ClockCycles(dut.clk,20)
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value==8
+
+@cocotb.test()
+async def test_mx8(dut):
+    dut._log.info("max 1")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    dut.uio_in.value = 0b0000_0_1_01
+    dut.ui_in.value = 0b0001_0000
+    await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_01
+    await ClockCycles(dut.clk,20)
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value== 1
+
+####################################################
+
+@cocotb.test()
+async def test_madd1(dut):
+    dut._log.info("max 7*1 + 5*3")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value= 1
+    dut.uio_in.value = 0b0000_0_1_10
+    dut.ui_in.value = 0b0111_0001
+    await ClockCycles(dut.clk,1 )
+    dut.ui_in.value = 0b0011_0101
+    await ClockCycles(dut.clk,1 )
+    
+    dut.uio_in.value = 0b0000_1_0_10
+    await ClockCycles(dut.clk,20)
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value== 22
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+async def test_madd2(dut):
+    dut._log.info("max 1*1 + 15*1")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
     dut.rst_n.value = 1
+    dut.uio_in.value = 0b0000_0_1_10
+    dut.ui_in.value = 0b1110_0001
+    await ClockCycles(dut.clk,1 )
+    dut.ui_in.value = 0b0001_0010
+    await ClockCycles(dut.clk,1 )
+    
+    dut.uio_in.value = 0b0000_1_0_10
+    await ClockCycles(dut.clk,20)
+    dut._log.info(dut.uo_out.value)
+    assert dut.uo_out.value== 16
 
-    dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+@cocotb.test()
+async def test_madd_full(dut):
+    dut._log.info("a n num value dot product")
+  
+    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+    dut.rst_n.value = 0
+    dut.uio_in.value = 0b0000_0_0_00
+    await ClockCycles(dut.clk,10)
+    # Reset
+    dut.ena.value = 1
+    dut.rst_n.value= 1
+    dot = 0 
+    for _ in range(50):
+        w = random.randint(0,14)
+        i = random.randint(0,15)
+        print(w,i)
+        v = (w<<4)+i
+        dot += w * i 
+        dut.uio_in.value = 0b0000_0_1_10
+        dut.ui_in.value = v
+        await ClockCycles(dut.clk,1 )
+    dut.uio_in.value = 0b0000_1_0_10
+    await ClockCycles(dut.clk,20)
+    dut._log.info(dot)
+    dut._log.info(int(dut.uo_out.value))
+    dut._log.info(int(dut.uio_out.value))
+    out_val = dut.uio_out.value<<4 | dut.uo_out.value
+    assert out_val == dot
